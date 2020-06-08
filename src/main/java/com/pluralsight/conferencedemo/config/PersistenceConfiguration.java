0bf35348881681ef168d80b239eb63b15c2cf6b1
Saxena <Ritika.Saxena@libertymutual.com>
2020-06-08 18:28:23 -0400

    First Commit

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,18 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url ("jdbc:postgresql://localhost:5432/conference_app");
+        System.out.println("Custom bean has been set");
+        return builder.build();
+    }
+}
